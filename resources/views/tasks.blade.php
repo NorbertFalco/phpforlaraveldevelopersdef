12026cc90627c41b1fafd750ea4b69671f7c2953
Norbert Falcó <norbertfalco@gmail.com>
2023-10-30 20:25:16 +0100

    Sol·lucio als exercicis proposats

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -14,11 +14,11 @@
             <div class="px-4 sm:px-6 lg:px-8">
                 <div class="sm:flex sm:items-center">
                     <div class="sm:flex-auto">
-                        <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
-                        <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
+                        <h1 class="text-base font-semibold leading-6 text-gray-900">Tasks</h1>
+                        <p class="mt-2 text-sm text-gray-700">A list of all the tasks in the database</p>
                     </div>
                     <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add user</button>
+                        <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add task</button>
                     </div>
                 </div>
                 <div class="mt-8 flow-root">
